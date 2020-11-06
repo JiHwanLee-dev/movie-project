b9e59a4882fc873d74bf3974ca08588a5273c40e
JihwanLee <kdgosu53@gmail.com>
2020-11-06 19:35:23 +0900

    login-degisn-working

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,35 +16,21 @@
     <v-main>
         <v-container
         fluid
+        
         >
-          <v-card>
-            <v-tabs
-            v-model="tab"
-            background-color="primary"
-            dark
-            >
-            <v-tab
-                v-for="item in items"
-                :key="item.tab"
-            >
-                {{ item.tab }}
-            </v-tab>
-            </v-tabs>
+          
 
-            <v-tabs-items v-model="tab">
-            <v-tab-item
-                v-for="item in items"
-                :key="item.tab"
+        <v-row
+            align="center"
+            justify="center"
+            
+        >
+            <v-col
+            
+                cols="12"
+                sm="6"
+               
             >
-                <v-card flat>
-                <v-card-text>{{ item.content }}</v-card-text>
-                </v-card>
-            </v-tab-item>
-            </v-tabs-items>
-        </v-card>
-
-        <v-row>
-            <v-col>
                 <v-text-field
                     outlined
                     dense   
@@ -58,7 +44,26 @@
                     label="비밀번호"
                 >
                 </v-text-field>
+
+                <v-checkbox
+                   label="아이디 저장"
+                >
+
+                </v-checkbox>
+
+                <v-btn
+                    
+                    color="primary"
+                >
+                    로그인
+                </v-btn>
+
+                
             </v-col>
+
+            
+
+            
         </v-row>
 
        
